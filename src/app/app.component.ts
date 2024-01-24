510a7240e9ac1eecbe1d272c8633a209889bc52c
Jeremias Puerta <jeremiaspuerta12@gmail.com>
2024-01-24 13:18:09 -0300

    lesson 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   styleUrl: './app.component.css',
-  template: `<h1>Hello world!</h1>`
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
 })
 export class AppComponent {
   title = 'homes';
